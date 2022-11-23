a413759ba0424a2f24f65048669ff489b8062d4c
Nayelly <nayellypantoja2000@gmail.com>
2022-11-23 09:30:49 -0500

    resolución clase1

--- clase1.js
+++ clase1.js
@@ -6,9 +6,19 @@ function iniciarJuego() {
     // suludamos al usuario
     alert("Bienvenido al piedra papel o tijera de Frontend II.");
     // guardamos en una variable en nombre ingresado
-    const nombre = prompt("Ingese su nombre por favor:")
+    let nombre; 
 
-    alert("Gracias por jugar " + nombre + ". ¡Mucha suerte!");
+    do{
+        nombre = prompt("Ingrese su nombre por favor:");
+        
+        if(nombre.length > 3 && isNaN(nombre)){
+            alert("Gracias por jugar " + nombre.toUpperCase() + ". ¡Mucha suerte!");
+        }else{
+            alert("El dato ingresado es invalido")
+        }
+    }while(nombre.length < 3 || !isNaN(nombre) || nombre == null || nombre == "")
+
+
 
     // mostramos los datos por consola
     console.log("----------------------------");
@@ -16,7 +26,7 @@ function iniciarJuego() {
     console.log(nombre);
     console.log("----------------------------");
 
-    return nombre;
+    return nombre.toUpperCase;
 }
 
 // creamos una variable a nivel global para guardar el nombre del jugador que nos devuelve la función
@@ -28,5 +38,7 @@ const nombreJugador = iniciarJuego();
 // 1- Modificar la funcion de iniciarJuego(), validar si ingresa un dato válido como nombre.
 // 2- Si no ingresa un texto, o tiene menos de 3 caracteres debemos volverle a pedir que lo ingrese.
 // 3- Finalmente el nombre devuelto debe estar todo en mayúsculas.
+
+
 
 
